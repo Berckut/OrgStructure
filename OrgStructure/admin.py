cd7d86e64463311c99b4e920e02fb09d7d1b8a5f
Berckut <berckut@rambler.ru>
2022-03-27 20:57:33 +0700

    Реализован базовый функционал, база наполнена событиями.

--- OrgStructure/admin.py
+++ OrgStructure/admin.py
@@ -1,3 +1,236 @@
 from django.contrib import admin
+from . import models
+import random
 
-# Register your models here.
+
+# Вывод подразделений, созданных в результате структурного изменения
+class OrgUnitCreationInline(admin.TabularInline):
+    model = models.OrgUnit
+    fk_name = 'reason_creation'
+    fields = (
+        'exist',
+        'structure_code',
+        'short_name',
+        'name',
+        'type_of_unit',
+    )
+    readonly_fields = (
+        'exist',
+        'structure_code',
+        'short_name',
+        'name',
+        'type_of_unit',
+    )
+    extra = 0
+    verbose_name_plural = 'Созданные подразделения'
+    can_delete = False
+    can_add = False
+    show_change_link = True
+
+    def has_add_permission(self, request, obj): return False
+
+
+# Вывод подразделений, упразднённых в результате структурного изменения
+class OrgUnitAbolitionInline(admin.TabularInline):
+    model = models.OrgUnit
+    fk_name = 'reason_abolition'
+    fields = (
+        'exist',
+        'structure_code',
+        'short_name',
+        'name',
+        'type_of_unit',
+    )
+    readonly_fields = (
+        'exist',
+        'structure_code',
+        'short_name',
+        'name',
+        'type_of_unit',
+    )
+    extra = 0
+    verbose_name_plural = 'Упразднённые подразделения'
+    can_delete = False
+    can_add = False
+    show_change_link = True
+
+    def has_add_permission(self, request, obj): return False
+
+
+@admin.register(models.Change)
+class ChangeAdmin(admin.ModelAdmin):
+    # Запись
+    date_hierarchy = 'date'
+
+    fields = (
+        'date',
+        'change',
+        'event',
+        'note',
+    )
+
+    inlines = [
+        OrgUnitCreationInline,
+        OrgUnitAbolitionInline,
+    ]
+
+    # Список
+
+    # Вывод форматированного поля даты создания
+    def view_list_display_change_date(self, obj):
+        if obj.date:
+            return obj.date.strftime("%d.%m.%Y")
+
+    view_list_display_change_date.short_description = 'Дата'
+
+    list_display = (
+        'view_list_display_change_date',
+        'event',
+        'change',
+    )
+
+    list_display_links = (
+        'event',
+    )
+
+    list_filter = (
+        'date',
+        'change',
+    )
+
+    search_fields = (
+        'event',
+        'search_line',
+    )
+
+    # Отключить действия
+    actions = None
+
+
+# Вывод подчинённых подразделений
+class OrgUnitSubjectsInline(admin.TabularInline):
+    model = models.OrgUnit
+    fk_name = 'parent'
+    fields = (
+        'exist',
+        'structure_code',
+        'short_name',
+        'name',
+        'date_creation',
+        'date_abolition',
+    )
+    readonly_fields = (
+        'exist',
+        'structure_code',
+        'short_name',
+        'name',
+        'date_creation',
+        'date_abolition',
+    )
+    ordering = [
+        '-exist',
+        'name',
+    ]
+    extra = 0
+    verbose_name_plural = 'Подчинённые подразделения'
+    can_delete = False
+    can_add = False
+    show_change_link = True
+
+    def has_add_permission(self, request, obj): return False
+
+
+# Подразделение
+@admin.register(models.OrgUnit)
+class OrgUnitAdmin(admin.ModelAdmin):
+    # Запись
+    fieldsets = (
+        ('Параметры подразделения', {
+            'fields': (
+                'type_of_unit',
+                'name',
+                'short_name',
+                'structure_code',
+                'parent',
+                'exist',
+            )
+        }),
+        ('Сведения о создании подразделения', {
+            'fields': (
+                'reason_creation',
+                'date_creation',
+            )
+        }),
+        ('Сведения об упразднении подразделения', {
+            'fields': (
+                'reason_abolition',
+                'date_abolition',
+            )
+        }),
+    )
+
+    raw_id_fields = (
+        'parent',
+        'reason_creation',
+        'reason_abolition',
+    )
+
+    readonly_fields = (
+        'date_creation',
+        'date_abolition',
+        'exist',
+    )
+
+    inlines = [
+        OrgUnitSubjectsInline,
+    ]
+
+    # Список
+
+    # Вывод форматированного поля даты создания
+    def view_list_display_date_creation(self, obj):
+        if obj.date_creation:
+            return obj.date_creation.strftime("%d.%m.%Y")
+
+    view_list_display_date_creation.empty_value_display = '?'
+    view_list_display_date_creation.short_description = 'Дата создания'
+
+    # Вывод форматированного поля даты упразднения
+    def view_list_display_date_abolition(self, obj):
+        if obj.date_abolition:
+            return obj.date_abolition.strftime("%d.%m.%Y")
+
+    view_list_display_date_abolition.empty_value_display = ''
+    view_list_display_date_abolition.short_description = 'Дата упразднения'
+
+    list_display = (
+        'exist',
+        'structure_code',
+        'short_name',
+        'name',
+        'type_of_unit',
+        'view_list_display_date_creation',
+        'view_list_display_date_abolition',
+    )
+
+    list_display_links = (
+        'name',
+        'short_name',
+    )
+
+    list_filter = (
+        'exist',
+        'type_of_unit',
+        'date_creation',
+        'date_abolition',
+    )
+
+    search_fields = (
+        'name',
+        'short_name',
+        'structure_code',
+        'search_line'
+    )
+
+    # Отключить действия
+    actions = None
--- OrgStructure/models.py
+++ OrgStructure/models.py
@@ -1,3 +1,257 @@
 from django.db import models
+import datetime
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class TypeOfOrgUnit(models.TextChoices):
+    """
+    Виды структурных единиц
+    """
+    DIVISION = 'dv', 'Обособленное подразделение'
+    OFFICIAL = 'of', 'Куратор'
+    BODY = 'bd', 'Орган'
+
+
+class TypeOfReorganization(models.TextChoices):
+    """
+    Виды структурных изменений (реорганизации)
+    """
+    CREATION = 'cr', 'Создание'
+    ABOLITION = 'ab', 'Упразднение'
+    REASSIGN = 'rs', 'Переподчинение'
+    RENAME = 'rn', 'Переименование'
+    MERGER = 'mr', 'Слияние'
+    DIVISION = 'dv', 'Разделение'
+
+
+class Change(models.Model):
+    """
+    Изменение структуры (реорганизация)
+    """
+
+    # Вид реорганизации
+    change = models.CharField(
+        choices=TypeOfReorganization.choices,
+        max_length=2,
+        verbose_name='Вид реорганизации',
+        blank=False,
+    )
+
+    # Дата события
+    date = models.DateField(
+        verbose_name='Дата события',
+        default=datetime.date.today,
+        blank=False,
+    )
+
+    # Событие
+    event = models.CharField(
+        verbose_name='Событие',
+        max_length=64,
+        null=True,
+        blank=False,
+    )
+
+    # Описание события
+    note = models.TextField(
+        verbose_name='Описание события',
+        null=True,
+        blank=True,
+    )
+
+    # Поле для поиска (содержит запись события в нижнем регистре)
+    search_line = models.CharField(
+        verbose_name='Поле для поиска',
+        help_text='Содержит запись события в нижнем регистре',
+        max_length=254,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'Изменение структуры'
+        verbose_name_plural = 'Структурные изменения'
+        ordering = [
+            '-date',
+            'change',
+            'event',
+        ]
+
+    def __repr__(self):
+        return (f'{self.__class__.__name__}('
+                f'{self.change}, {self.date.strftime("%Y.%m.%d")}, "{self.event}")')
+
+    def __str__(self):
+        return f'{self.date.strftime("%Y.%m.%d")} {self.event}'
+
+
+# Установить значения для автоматических полей
+@receiver(pre_save, sender=Change)
+def change_pre_save(instance, **kwargs):
+    # Сформировать поле поиска
+    instance.search_line = f'{instance.event.lower()}'[:254]
+
+
+class OrgUnit(models.Model):
+    """
+    Подразделение
+    """
+
+    # Наименование
+    name = models.CharField(
+        verbose_name='Наименование',
+        help_text='Полное наименование подразделения',
+        max_length=200,
+        null=True,
+        blank=False,
+    )
+
+    # Сокращённое наименование
+    short_name = models.CharField(
+        verbose_name='Аббревиатура',
+        help_text='Краткое наименование подразделения',
+        max_length=32,
+        null=True,
+        blank=True,
+    )
+
+    # Код подразделения
+    structure_code = models.CharField(
+        verbose_name='Код',
+        help_text='Код структурного подразделения',
+        max_length=32,
+        null=True,
+        blank=True,
+    )
+
+    # Вид подразделения
+    type_of_unit = models.CharField(
+        choices=TypeOfOrgUnit.choices,
+        max_length=2,
+        verbose_name='Вид',
+        help_text='Вид подразделения',
+        default='dv',
+        blank=False,
+    )
+
+    # Причина создания
+    reason_creation = models.ForeignKey(
+        Change,
+        verbose_name='Событие',
+        related_name='org_unit_creation',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    # Дата создания (устанавливается по причине создания)
+    date_creation = models.DateField(
+        verbose_name='Дата создания',
+        null=True,
+        blank=True,
+    )
+
+    # Причина упразднения
+    reason_abolition = models.ForeignKey(
+        Change,
+        verbose_name='Событие',
+        related_name='org_unit_abolition',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    # Дата упразднения (устанавливается по причине упразднения)
+    date_abolition = models.DateField(
+        verbose_name='Дата упразднения',
+        null=True,
+        blank=True,
+    )
+
+    # Подразделение существует или упразднено? (устанавливается по причине упразднения)
+    exist = models.BooleanField(
+        verbose_name='Статус',
+        help_text='Подразделение существует или упразднено?',
+        default=True,
+    )
+
+    # Подчинение
+    parent = models.ForeignKey(
+        'self',
+        verbose_name='Подчинение',
+        related_name='org_unit_parent',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
+
+    # Поле для поиска (содержит другие ключевые поля в нижнем регистре)
+    search_line = models.CharField(
+        verbose_name='Поле для поиска',
+        help_text='Содержит все ключевые поля в нижнем регистре',
+        max_length=254,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'подразделение'
+        verbose_name_plural = 'Подразделения'
+        ordering = [
+            'name',
+        ]
+        unique_together = ['name', 'reason_creation']
+
+    def __repr__(self):
+        result = f'{self.__class__.__name__}('
+
+        if self.short_name:
+            result += f'{self.short_name}, '
+        else:
+            result += f'{self.name}, '
+
+        return result
+
+    def __str__(self):
+        return self.name
+
+
+# Установить значения для автоматических полей
+@receiver(pre_save, sender=OrgUnit)
+def orgunit_pre_save(instance, **kwargs):
+    # Получить дату создания подразделения
+    if instance.reason_creation:
+        instance.date_creation = instance.reason_creation.__getattribute__('date')
+
+    # Получить дату упразднения подразделения
+    if instance.reason_abolition:
+        instance.date_abolition = instance.reason_abolition.__getattribute__('date')
+
+    # Определить статус подразделения
+    if instance.reason_abolition:
+        instance.exist = False
+    else:
+        instance.exist = True
+
+    # Сформировать поле поиска
+    sl = f''
+    if instance.short_name:
+        sl += f'{instance.short_name.lower()}'
+    if instance.structure_code:
+        sl += f'{instance.structure_code.lower()}'
+    if instance.name:
+        sl += f'{instance.name.lower()}'
+    instance.search_line = sl[:254]
+
+
+# Корректировка полей даты создания и упразднения подразделения при изменении записи реорганизации
+@receiver(post_save, sender=Change)
+def update_orgunit_date_after_change_post_save(instance, **kwargs):
+    # Корректировка даты создания подразделения
+    for ou in instance.org_unit_creation.all():
+        ou.save()
+
+    # Корректировка даты упразднения подразделения
+    for ou in instance.org_unit_abolition.all():
+        ou.save()
